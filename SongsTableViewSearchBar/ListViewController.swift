f231238a3f5d72cb145e00023c3db1c6e8e276ae
Matthew Ramos <mmramos309@gmail.com>
2019-11-27 14:17:39 -0500

    Populated table view, added and began set up on search bar delegate functions

---
+++ SongsTableViewSearchBar/ListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SongsTableViewSearchBar
+//
+//  Created by C4Q  on 11/6/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+
+}
+
